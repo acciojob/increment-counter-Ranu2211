9f97837f2912eb9ec0e9330e3c187d864ac3a339
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-03 19:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const button = document.getElementById('btn');
+const button = document.getElementById('incrementBtn');
 const counter =  document.getElementById('counter');
 let count = 0;
 counter.textContent = count;
